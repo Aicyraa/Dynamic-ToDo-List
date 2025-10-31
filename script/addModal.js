5cd7f36221283b9e30707adbb7a10006dea2e888
BFP <182109647+Aicyraa@users.noreply.github.com>
2025-10-31 13:39:01 +0800

    add modal modulee

---
+++ script/addModal.js
@@ -0,0 +1,19 @@
+let add_modal = {
+   overlay: document.querySelector(".overlay"),
+   addModal: document.querySelector(".header__add"),
+   addBtn: document.getElementById("header__add__btn"),
+   saveBtn: document.querySelector("#header__add__submit"),
+};
+
+function openModal() {
+   add_modal.overlay.classList.add("active");
+   add_modal.addModal.classList.add("active");
+}
+
+function closeModal() {
+   add_modal.overlay.classList.remove("active");
+   add_modal.addModal.classList.remove("active");
+}
+
+export default add_modal
+export {openModal, closeModal}
