e366a32d52782fd8570779aa371dfba0f6f58425
Jee <182109647+Aicyraa@users.noreply.github.com>
2025-11-01 19:05:08 +0800

    updated the syntax

--- script/script.js
+++ script/script.js
@@ -3,21 +3,23 @@ import { setID as id, removeValue as remove} from "./taskHelpers.js";
 import modal, {
    openModal as open,
    closeModal as close,
+   setError as err
 } from "./modals.js";
 
 // Modal
 modal.addBtn.addEventListener("click", open(modal.addModal, modal.overlay));
 modal.closeBtn.addEventListener("click", close(modal.addModal, modal.overlay))
+modal.errBtn.addEventListener("click", close(modal.errModal))
 
 // Add Task
 modal.saveBtn.addEventListener("click", (e) => {
 
    let task = document.getElementById("add__task");
    let deadline = document.getElementById("add__task__due");
-   let close = close(modal.addModal, modal.overlay)
+   let btn = close(modal.addModal, modal.overlay)
 
    if (!task.value.trim() || !deadline.value.trim()) {
-      return alert("invalid task!");
+      return err('Invalid Input!')
    }
 
    let storage = JSON.parse(localStorage.getItem('storage')) || []
@@ -25,6 +27,6 @@ modal.saveBtn.addEventListener("click", (e) => {
    localStorage.setItem('storage', JSON.stringify(storage))
    
    remove(task, deadline)
-   close()
+   btn()
 
 });
